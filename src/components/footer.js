826a38c2749b0c1a8965a7d185081662b214f691
Raphael Rychetsky <raphael@rycode.de>
2020-08-01 20:25:44 +0100

    footer styling

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,11 @@ import styled from "styled-components"
 import tw from 'twin.macro'
 
 const SiteFooter = styled.footer`
-  ${tw`p-4 py-8 mt-8 text-lg leading-loose text-center bg-gray-100 lg:px-16 dark:bg-gray-800 dark:text-white`}
+  ${tw`p-4 py-8 mt-8 leading-loose text-center bg-gray-100 lg:px-16 dark:bg-gray-800 dark:text-white`}
+
+  & a {
+    ${tw`font-medium`}
+  }
 `
 
 const Footer = ({ content }) => (
